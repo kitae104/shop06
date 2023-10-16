361e799c5245c8f84b1ab687f3923af40a3f3ba5
Kitae's Github <aqua0405@gmail.com>
2023-10-16 19:43:49 +0900

    Create ItemImgRepository.java

---
+++ src/main/java/kr/inhatc/shop/repository/ItemImgRepository.java
@@ -0,0 +1,7 @@
+package kr.inhatc.shop.repository;
+
+import kr.inhatc.shop.entity.ItemImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+}
