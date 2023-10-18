8a125c390597ae842d62a2664073aa8828401efe
Kitae's Github <aqua0405@gmail.com>
2023-10-18 23:37:12 +0900

    Update ItemImgRepository.java

--- src/main/java/kr/inhatc/shop/repository/ItemImgRepository.java
+++ src/main/java/kr/inhatc/shop/repository/ItemImgRepository.java
@@ -2,6 +2,12 @@ package kr.inhatc.shop.repository;
 
 import kr.inhatc.shop.entity.ItemImg;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ItemImgRepository extends JpaRepository<ItemImg, Long> {
+
+    //@Query("select i from ItemImg i where i.item.id = :itemId order by i.id asc")
+    List<ItemImg> findByItemIdOrderByIdAsc(Long itemId);  // 상품 이미지 리스트 가져오기
 }
