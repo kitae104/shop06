cdea37d0f6130b0f46376f640797f8a0510893ae
Kitae's Github <aqua0405@gmail.com>
2023-10-18 23:37:09 +0900

    Update ItemController.java

--- src/main/java/kr/inhatc/shop/controller/ItemController.java
+++ src/main/java/kr/inhatc/shop/controller/ItemController.java
@@ -1,5 +1,6 @@
 package kr.inhatc.shop.controller;
 
+import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import kr.inhatc.shop.dto.ItemFormDto;
 import kr.inhatc.shop.service.ItemService;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
@@ -48,5 +50,19 @@ public class ItemController {
         }
 
         return "redirect:/";
+    }
+
+    @GetMapping("/admin/item/{itemId}")
+    public String itemDetail(@PathVariable("itemId") Long itemId, Model model) {
+
+        try{
+            ItemFormDto itemFormDto = itemService.getItemDetail(itemId);            // 상품 정보 가져오기
+            model.addAttribute("itemFormDto", itemFormDto);       // 상품 정보
+            return "item/itemForm";
+        } catch (EntityNotFoundException e){
+            model.addAttribute("errorMessage", "존재하지 않는 상품입니다.");
+            model.addAttribute("itemFormDto", new ItemFormDto());
+            return "item/itemForm";
+        }
     }
 }
