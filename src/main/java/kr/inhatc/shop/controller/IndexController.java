d7e12a2525540c58e01c94a9d422029762ffc85d
Kitae's Github <aqua0405@gmail.com>
2023-10-21 23:49:14 +0900

    Update IndexController.java

--- src/main/java/kr/inhatc/shop/controller/IndexController.java
+++ src/main/java/kr/inhatc/shop/controller/IndexController.java
@@ -1,7 +1,5 @@
 package kr.inhatc.shop.controller;
 
-import kr.inhatc.shop.config.auth.PrincipalDetails;
-import kr.inhatc.shop.entity.Member;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,13 +9,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class IndexController {
 
     @GetMapping("/")
-    public String index(@AuthenticationPrincipal PrincipalDetails userDetails, Model model) {
-        if(userDetails != null) {
-            Member member = userDetails.getMember();
-            model.addAttribute("message", "사용자 이름은 " + member.getName() + "이고, 권한은 " + member.getRole().toString() + " 입니다.");
-        } else {
-            model.addAttribute("message", "로그인 전 입니다.");
-        }
+    public String index() {
         return "index";
     }
 }
