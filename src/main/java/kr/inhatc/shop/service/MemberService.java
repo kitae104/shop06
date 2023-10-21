b2b7eb6e9c2d0187172b42db071df8f0496bc47c
Kitae's Github <aqua0405@gmail.com>
2023-10-21 23:49:31 +0900

    Update MemberService.java

--- src/main/java/kr/inhatc/shop/service/MemberService.java
+++ src/main/java/kr/inhatc/shop/service/MemberService.java
@@ -3,6 +3,10 @@ package kr.inhatc.shop.service;
 import kr.inhatc.shop.entity.Member;
 import kr.inhatc.shop.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +15,7 @@ import java.util.Optional;
 @Service
 @Transactional
 @RequiredArgsConstructor
-public class MemberService {
+public class MemberService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
@@ -26,5 +30,18 @@ public class MemberService {
             System.out.println("member : " + findMember);
             throw new IllegalStateException("이미 가입된 회원입니다.");
         }
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+
+        Member member = memberRepository.findByEmail(email).orElseThrow(() ->
+                new UsernameNotFoundException("해당 사용자를 찾을 수 없습니다. : " + email));
+
+        return User.builder()
+                .username(member.getEmail())
+                .password(member.getPassword())
+                .roles(member.getRole().toString())
+                .build();
     }
 }
