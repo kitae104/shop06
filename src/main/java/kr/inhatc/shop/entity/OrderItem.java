5b23e188c34b7d52b7a9039e287e4b2d72eaa5c3
Kitae's Github <aqua0405@gmail.com>
2023-10-30 02:23:45 +0900

    Update OrderItem.java

--- src/main/java/kr/inhatc/shop/entity/OrderItem.java
+++ src/main/java/kr/inhatc/shop/entity/OrderItem.java
@@ -16,17 +16,17 @@ public class OrderItem extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "order_item_id")
-    private Long id;                // 주문 상품 코드
+    private Long id;                        // 주문 상품 코드
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "order_id")
-    private Order order;            // 주문
+    @ManyToOne(fetch = FetchType.LAZY)      // 다대일 관계이므로 연관관계의 주인은 다(N)쪽에 설정한다.
+    @JoinColumn(name = "order_id")          // FK를 지정한다.
+    private Order order;                    // 주문
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "item_id")
-    private Item item;              // 주문 상품
+    @ManyToOne(fetch = FetchType.LAZY)      // 다대일 관계이므로 연관관계의 주인은 다(N)쪽에 설정한다.
+    @JoinColumn(name = "item_id")           // FK를 지정한다.
+    private Item item;                      // 주문 상품
 
-    private int orderPrice;         // 주문 가격
+    private int orderPrice;                 // 주문 가격
 
-    private int count;              // 주문 수량
+    private int count;                      // 주문 수량
 }
