ee7a4c53678c1963d14f8684ae64114bd5b6551c
Kitae's Github <aqua0405@gmail.com>
2023-11-08 02:06:38 +0900

    Update BaseEntity.java

--- src/main/java/kr/inhatc/shop/utils/audit/BaseEntity.java
+++ src/main/java/kr/inhatc/shop/utils/audit/BaseEntity.java
@@ -10,15 +10,15 @@ import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @EntityListeners(value = {AuditingEntityListener.class})
-@MappedSuperclass
+@MappedSuperclass                   // 테이블로 매핑하지 않고, 자식 Entity에게 매핑 정보를 상속하기 위한 어노테이션
 @Getter
 @Setter
 public abstract class BaseEntity extends BaseTimeEntity {
 
-    @CreatedBy
-    @Column(updatable = false)
-    private String createdBy;      // 등록자
+    @CreatedBy                      // 생성자
+    @Column(updatable = false)      // 수정 불가
+    private String createdBy;       // 등록자
 
-    @LastModifiedBy
+    @LastModifiedBy                 // 최종 수정자
     private String modifiedBy;      // 수정자
 }
