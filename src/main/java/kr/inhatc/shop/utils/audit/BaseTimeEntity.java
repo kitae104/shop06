659c6e641d3e51cdca338ea8e2afed84d4c0bfe8
Kitae's Github <aqua0405@gmail.com>
2023-11-08 02:06:44 +0900

    Update BaseTimeEntity.java

--- src/main/java/kr/inhatc/shop/utils/audit/BaseTimeEntity.java
+++ src/main/java/kr/inhatc/shop/utils/audit/BaseTimeEntity.java
@@ -12,15 +12,15 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import java.time.LocalDateTime;
 
 @EntityListeners(value = {AuditingEntityListener.class})
-@MappedSuperclass
+@MappedSuperclass                       // 테이블로 매핑하지 않고, 자식 Entity에게 매핑 정보를 상속하기 위한 어노테이션
 @Getter
 @Setter
 public abstract class BaseTimeEntity{
 
-    @CreatedDate
-    @Column(updatable = false)
+    @CreatedDate                        // 최초 생성 시점
+    @Column(updatable = false)          // 수정 불가
     private LocalDateTime regTime;      // 등록일
 
-    @LastModifiedDate
+    @LastModifiedDate                   // 최종 수정 시점
     private LocalDateTime updateTime;   // 수정일
 }
