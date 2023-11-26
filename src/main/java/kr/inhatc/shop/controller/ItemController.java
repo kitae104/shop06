a78e01bcbe124c02559ba9429683f3be689e3b2a
Kitae's Github <aqua0405@gmail.com>
2023-11-27 02:52:29 +0900

    Update ItemController.java

--- src/main/java/kr/inhatc/shop/controller/ItemController.java
+++ src/main/java/kr/inhatc/shop/controller/ItemController.java
@@ -3,8 +3,14 @@ package kr.inhatc.shop.controller;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.validation.Valid;
 import kr.inhatc.shop.dto.ItemFormDto;
+import kr.inhatc.shop.dto.ItemSearchDto;
+import kr.inhatc.shop.entity.Item;
 import kr.inhatc.shop.service.ItemService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -16,6 +22,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
@@ -90,8 +97,16 @@ public class ItemController {
         return "redirect:/";
     }
 
-    @GetMapping("/admin/items")
-    public String itemList() {
+    @GetMapping({"/admin/items", "/admin/items/{page}"})
+    public String itemList(ItemSearchDto itemSearchDto, Model model, @PathVariable(value = "page")Optional<Integer> page){
+        // 페이지 정보 (0번째 페이지, 10개씩, id 내림차순)
+        Pageable pageable = PageRequest.of(page.isPresent()? page.get(): 0, 5, Sort.by("id").descending());
+        Page<Item> items = itemService.getAdminItemPage(itemSearchDto, pageable);
+
+        model.addAttribute("items", items);
+        model.addAttribute("itemSearchDto", itemSearchDto);
+        model.addAttribute("maxPage", 5);
+
         return "item/itemList";
     }
 }
