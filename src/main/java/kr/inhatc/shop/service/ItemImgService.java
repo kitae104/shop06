fc2bc2bb401ac5ef77234c47350038cd9b78afa2
Kitae's Github <aqua0405@gmail.com>
2024-12-06 00:17:20 +0900

    Update ItemImgService.java

--- src/main/java/kr/inhatc/shop/service/ItemImgService.java
+++ src/main/java/kr/inhatc/shop/service/ItemImgService.java
@@ -14,8 +14,9 @@ import java.io.IOException;
 
 @Service
 @RequiredArgsConstructor
-@Transactional
+@Transactional // 트랜잭션 처리
 public class ItemImgService {
+
     @Value(value = "${itemImgLocation}")
     private String itemImgLocation;
 
@@ -40,18 +41,20 @@ public class ItemImgService {
 
     public void updateItemImg(Long ItemImgId, MultipartFile itemImgFile) throws IOException {
         if(!itemImgFile.isEmpty()){
+            // 해당 이미지 정보 가져오기
             ItemImg itemImg = itemImgRepository.findById(ItemImgId).orElseThrow(EntityNotFoundException::new);
 
-            // 파일 이름이 존재하는 경우
+            // 파일 이름이 존재하는 경우란면 기존 파일 삭제 후 처리
             if(!StringUtils.isEmpty(itemImg.getImgName())) {
                 fileService.deleteFile(itemImgLocation + "/" + itemImg.getImgName());   // 기존 파일 삭제
             }
 
+            // 새로운 파일 업로드 처리
             String originalFileName = itemImgFile.getOriginalFilename();
             String imgNamae = fileService.uploadFile(itemImgLocation, originalFileName, itemImgFile.getBytes());
             String imgUrl = "/images/item/" + imgNamae;
 
-            // 더티체킹
+            // 더티체킹 - 저장 없이 엔티티 값만 변경하면 자동으로 업데이트 처리됨
             itemImg.updateItemImg(originalFileName, imgNamae, imgUrl);  // 파일 정보 업데이트
         }
     }
